d5975a11ac6de363464f638ed1ed54aaf9ead654
Jay <lingxiaohong2dai@gmail.com>
2015-03-06 16:00:45 +0000

    change homework1.R

--- homework/homework_1/problem_3/homework1.R
+++ homework/homework_1/problem_3/homework1.R
@@ -6,7 +6,7 @@ con_dply <- src_mysql(dbname="rstudio",user="root",password="lxhnt0906",host="lo
 
 myData <- con_dply %>%
   tbl("mratings") %>%
-  collect()
+  collect()user_ranking
 
 myData <- myData %>%
   group_by(MovieID) %>%
@@ -14,7 +14,7 @@ myData <- myData %>%
   mutate(inventory = min_rank(popularity)) %>%
   select(inventory,MovieID) %>%
   inner_join(myData)
-
+  
 myData <- myData %>%
   group_by(UserID) %>%
   summarise(total_rated = n()) %>%
@@ -27,29 +27,26 @@ min(myData$total_rated) < 10  #no one rated less than 10 movie
 #we need to pick the maximum inventory level for each user.
 
 
-satisfy_100<- myData %>%
+statisfy_100<- myData %>%
   group_by(UserID) %>%
   summarise(max_inv_level = max(inventory)) %>%
   arrange(max_inv_level) %>%
   mutate(cum= cume_dist(max_inv_level)) %>%
   distinct(max_inv_level) %>%
   select(max_inv_level,cum)
-
-
-qplot(x=max_inv_level, y=cum,data=satisfy_100,xlab="Movie Inventory",ylab="Percent of 100% Satisfied Users")
+  
+  
+qplot(x=max_inv_level, y=cum,data=statisfy_100,xlab="Movie Inventory",ylab="Percent of 100% Satisfied Users")
 
 prepare_90 <- myData %>%
   group_by(UserID) %>%
-  summarise(max_inv_level_90 = 0.9 * max(inventory)) %>%
-  inner_join(myData)
+  arrange(inventory) %>%
+  summarise(total=n(),invent_size = inventory[round(total*0.9)])
 
 satisfy_90 <- prepare_90 %>%
-  mutate(fil = inventory-max_inv_level_90) %>%
-  filter(fil >= 0) %>%
-  group_by(UserID) %>%
-  summarise(min_inv = min(inventory)) %>%
-  arrange(min_inv) %>%
-  mutate(cum = cume_dist(min_inv)) %>%
-  select(min_inv,cum)
+  arrange(invent_size) %>%
+  mutate(cum = cume_dist(invent_size)) %>%
+  select(invent_size,cum)
 
-qplot(x=min_inv, y=cum,data=satisfy_90,xlab="Movie Inventory",ylab="Percent of 90% Satisfied Users")
+qplot(x=invent_size, y=cum,data=satisfy_90,xlab="Movie Inventory",ylab="Percent of 90% Satisfied Users")
+
